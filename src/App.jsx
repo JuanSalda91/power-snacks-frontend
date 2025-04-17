9efb061b4f995716e4f2cc6fe8bb465328dc7d71
JuanSalda91 <jpsalda6@hotmail.com>
2025-04-16 22:15:50 -0400

    feat: Initial frontend setup using vite

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+      </div>
+      <h1>Power Snacks</h1>
+      <div>
+      </div>
+    </>
+  );
+}
+
+export default App
